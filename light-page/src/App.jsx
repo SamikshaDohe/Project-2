01a22cbff3d153050c42c24ade14ef5c7efbd83f
Samiksha-Dohe <samikshadohe@gmail.com>
2024-06-07 14:18:01 +0530

    change

---
+++ light-page/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+
+function App(){
+  return(
+    <div className = 'heading' >
+      <h2 className = 'text1'> This page is included in a free SaaS Website Kit. </h2> <h1 className = 'text2'> View the complete Kit </h1>
+    </div>
+  )
+}
+export default App
